8ebc10afb023b3bd2735f32a5a2003aa769ad593
Gaurav Singh <gaurav.singh2012@gmail.com>
2012-02-29 19:45:03 +0530

    new commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,10 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
+
+    flash[:error] = "Invalid email/password. New User Please Sign Up"  
+    render 'new'	
+      
     end
   end
 
